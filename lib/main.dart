01329d10b2cccac5ae3872db067ad005601671dd
BATTLE-STATION\dwiki <official.dwikiramdani@gmail.com>
2024-01-19 18:35:33 +0700

    Implement API Client for API request with Back End

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:zipkos_flutter/app.dart';
+import 'package:zipkos_flutter/helpers/api_client.dart';
 
 void main() {
+  ApiClient(baseUrl: 'https://api.example.com');
   runApp(const MyApp());
 }
